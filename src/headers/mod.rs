e884c2fa254e27123df5340beedf3bcfea0c88ee
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-01-25 12:27:26 +0000

    Fix 2 warnings in mod.rs

--- src/headers/mod.rs
+++ src/headers/mod.rs
@@ -4,11 +4,11 @@ mod ip;
 mod location;
 
 pub use self::auth::*;
-pub use self::cookie::*;
+
 pub use self::cookie::{Cookie, SetCookie}; // Needed to de-conflict glob import from headers;
 pub use self::ip::*;
 pub use self::location::Location; // Needed to de-conflict glob import from headers;
-pub use self::location::*;
+
 pub use headers::*;
 
 pub mod authorization {
